3f69be556c4a9f520e50b5009ecf07ceabe839b0
fwa7son <21fwatson@athenian.org>
2020-11-02 16:41:35 +0000

    in progress

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,19 @@
 public class NinthGrader {
-    
+    //private String firstName;
+    //private String lastName;
+
+    public Student(String firstName, String lastName){
+        super(firstName, lastName, 2024);
+    }
+    public Student(){
+        super("Finn", "Watson", 2024);
+    }
+    public void doSomething(){
+        System.out.println("I am working on a stats project");
+    }
+    public String toString(){
+        return this.getfirstName()+ " " + this.getlastName() + "," + this.getClassyear + "(ninth grader)";
+    }
+
+
 }
